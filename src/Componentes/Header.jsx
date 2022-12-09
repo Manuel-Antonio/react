19efd9cb390d0ac95417fa0d620dac08230b7ec7
Manuel-Antonio <manuelhuaman1998@gmail.com>
2022-12-09 18:12:24 -0500

    Vamos funciona

--- src/Componentes/Header.jsx
+++ src/Componentes/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <h1 className="font-black text-5xl text-center p-3 mt-5">
-            Seguimientos Pacientes {""}
+            Seguimientos Pacientes con GitHub New {""}
             <span className="text-indigo-600">Veterinaria</span>
         </h1>
     );
